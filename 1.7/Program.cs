f62db5f25a57f4b70d0df8fe3b4866e4ab057906
Benjamin Daniel Morgan <benjamindanielmorgan@benjamins-mbp.home>
2020-11-24 14:37:08 +0000

    first commit

---
+++ 1.7/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _1._7
+{
+    class Program
+    {
+        //author: Benjamin Daniel Morgan
+        //date: 04/12/17 
+
+        static void Main()
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Clear();
+
+            string input;
+            string planetname;
+            double radius;
+            const double Pi = 3.142;
+            double Volume;
+
+
+            Console.WriteLine("Hello, What Planet are you from?");
+            planetname = Console.ReadLine();
+
+            Console.Write("What is the planet's radius?:\t");
+            input = Console.ReadLine();
+            radius = Convert.ToDouble(input);
+
+            Volume = 4.0 / 3 * Pi * Math.Pow(radius, 3);
+            Volume = Math.Round(Volume, 2);
+
+            Console.WriteLine("Planet: " + planetname + "\t\t\tRadius: " + radius + " km");
+            Console.WriteLine("\t\tVolume of" + planetname);
+            Console.WriteLine("\t\t=======================");
+            Console.WriteLine("\t\t" + Volume + "cubic kilometres");
+        }
+    }
+}
